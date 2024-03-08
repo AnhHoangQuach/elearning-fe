5e993a46d5b8856aa5650a2afa71d6c697aac603
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-03-08 19:18:18 +0700

    fix: BlogPage

--- src/pages/MainPage/BlogPage/Blog.tsx
+++ src/pages/MainPage/BlogPage/Blog.tsx
@@ -28,7 +28,7 @@ const Blog: React.FC = () => {
       </form>
 
       <div>
-        <div className="text-center pt-16 md:pt-32">
+        <div className="text-center pt-16 md:pt-32 mb-32">
           <i
             className="fa-solid fa-trash icon-bin-blog"
             data-modal-target="popup-modal"
@@ -37,19 +37,19 @@ const Blog: React.FC = () => {
           <p className="text-sm md:text-base text-green-500 font-bold">
             04 AUGUST 2023 <span className="text-gray-900">/</span> BLOG DAILY
           </p>
-          <h1 className="font-bold break-normal text-3xl md:text-5xl">
+          <h1 className="font-bold break-normal text-3xl md:text-5xl ">
             Welcome to Ghostwind CSS
           </h1>
         </div>
-        <div
+        {/* <div
           className="container w-full max-w-6xl mx-auto bg-white bg-cover mt-8 rounded"
           style={{
             backgroundImage:
               "url('https://source.unsplash.com/collection/1118905/')",
             height: "75vh",
           }}
-        ></div>
-        <div className="container max-w-5xl mx-auto -mt-32">
+        ></div> */}
+        <div className="container max-w-5xl mx-auto -mt-32 mt-10">
           <div className="mx-0 sm:mx-6">
             <div
               className="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal"
