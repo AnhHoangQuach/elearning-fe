94e68012ff56d9dbbc3c3d5c2ef0aa6ec0c20e27
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-03-12 21:07:38 +0700

    fix: UI && ReloadPage

--- src/pages/AdminPage/AccountsManage/DeleteUser.tsx
+++ src/pages/AdminPage/AccountsManage/DeleteUser.tsx
@@ -1,17 +1,17 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { toast } from 'react-toastify'
-import adminApi from 'src/apis/adminApi'
-import ModalContainer from 'src/components/ModalContainer'
-import { isPending, isSuccess } from 'src/reducers/authSlice'
+import { Button } from "@mui/material";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import adminApi from "src/apis/adminApi";
+import ModalContainer from "src/components/ModalContainer";
+import { isPending, isSuccess } from "src/reducers/authSlice";
 
 interface DeleteAccountProps {
-  id: string | number
-  show?: boolean
-  isUpdate: (status: boolean) => void
-  setShow?: React.Dispatch<React.SetStateAction<boolean>>
-  onClose?: () => void
+  id: string | number;
+  show?: boolean;
+  isUpdate: (status: boolean) => void;
+  setShow?: React.Dispatch<React.SetStateAction<boolean>>;
+  onClose?: () => void;
 }
 
 const DeleteAccount: React.FC<DeleteAccountProps> = ({
@@ -21,38 +21,49 @@ const DeleteAccount: React.FC<DeleteAccountProps> = ({
   show = false,
   onClose,
 }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const handleDeleteAccount = async () => {
     // console.log("xoá user có id", id);
-    isUpdate(false)
-    dispatch(isPending())
+    isUpdate(false);
+    dispatch(isPending());
     try {
-      await adminApi.deleteUser(id)
+      await adminApi.deleteUser(id);
       // const response = await adminApi.deleteUser(id);
       // console.log("check delete3: ",response);
-      dispatch(isSuccess())
-      setShow?.(false)
-      isUpdate(true)
-      toast.success('Xoá tài khoản thành công', { position: 'bottom-right' })
+      dispatch(isSuccess());
+      setShow?.(false);
+      isUpdate(true);
+      toast.success("Xoá tài khoản thành công ", {
+        position: "bottom-right",
+      });
     } catch (error) {
-      console.log('lỗi rồi', { error })
-      dispatch(isSuccess())
-      setShow?.(false)
-      toast.warning('Xoá tài khoản thất bại', { position: 'bottom-right' })
+      console.log("lỗi rồi", { error });
+      dispatch(isSuccess());
+      setShow?.(false);
+      toast.warning("Xoá tài khoản thất bại", { position: "bottom-right" });
     }
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
   return (
-    <ModalContainer title="Bạn có chắc muốn xoá tài khoản này không?" open={show} onClose={onClose}>
+    <ModalContainer
+      title="Bạn có chắc muốn xoá tài khoản này khôngddd?"
+      open={show}
+      onClose={onClose}
+    >
       <Button variant="contained" color="warning" onClick={handleDeleteAccount}>
         Xoá tài khoản
       </Button>
-      <Button variant="contained" color="success" onClick={onClose} sx={{ marginLeft: 1 }}>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={onClose}
+        sx={{ marginLeft: 1 }}
+      >
         Huỷ bỏ
       </Button>
     </ModalContainer>
-  )
-}
+  );
+};
 
-export default DeleteAccount
+export default DeleteAccount;
